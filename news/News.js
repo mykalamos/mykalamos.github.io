52a491ec2ec0e5351ac0558a54b58cf715654f50
Riky Choudhuri <kalamos.github@outlook.com>
2022-12-29 23:38:04 +0000

    Automated commit on DESKTOP-4GC5RGD

--- news/News.js
+++ news/News.js
@@ -1,17 +1,17 @@
 ﻿function selectTab(evt, categoryName) {
-	var i, tabcontent, tablinks;
-	tabcontent = document.getElementsByClassName("tabcontent");
-	for (i = 0; i < tabcontent.length; i++) {
-		tabcontent[i].style.display = "none";
-	}
-	tablinks = document.getElementsByClassName("tablinks");
-	for (i = 0; i < tablinks.length; i++) {
-		tablinks[i].className = tablinks[i].className.replace(" active", "");
-	}
-	document.getElementById(categoryName).style.display = "block";
-	evt.currentTarget.className += " active";
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(categoryName).style.display = "block";
+    evt.currentTarget.className += " active";
 }
 
-function onLoadCompleted(event) {
-	selectTab(event, 'UKNews'); // TODO hardcoded
+function onLoadCompleted(event, divId) {
+    selectTab(event, divId);
 }
